3ed77719c7177f790496e2fe92256a87ed527f62
deepusiva1212 <148721856+deepusiva1212@users.noreply.github.com>
2026-04-26 22:12:23 +0530

    Add secure configuration for Deepu Siva Portal

---
+++ config.js
@@ -0,0 +1,9 @@
+// Deepu Siva Private Limited - Secure Configuration
+const CONFIG = {
+    DIRECTOR_UID: "JWR6AvoYHUUPchhCjQ9jFoJXk293",
+    DIRECTOR_EMAIL: "deepusiva947@gmail.com",
+    APP_ID: "1:1014351018202:web:16d2578f726159e5ef8de3",
+    PROJECT_NAME: "Deepu Siva Portal"
+};
+
+export default CONFIG;
