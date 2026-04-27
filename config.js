5debc4d6b95614bdf0cc1ba0b529502ca4d534dd
deepusiva1212 <148721856+deepusiva1212@users.noreply.github.com>
2026-04-27 22:26:42 +0530

    Fix duplicate DIRECTOR_EMAIL in config.js
    Removed duplicate DIRECTOR_EMAIL entry in config.

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 const CONFIG = {
-    
     DIRECTOR_EMAIL: "deepusiva947@gmail.com",
     WEB3_ACCESS_KEY: "59f7a0e9-fa46-4922-96e1-991f9e6cde5b",
-    
-    DIRECTOR_EMAIL: "deepusiva947@gmail.com",
     DIRECTOR_UID:   "JWR6AvoYHUUPchhCjQ9jFoJXk293",
     APP_ID:         "1:1014351018202:web:16d2578f726159e5ef8de3"
 };
